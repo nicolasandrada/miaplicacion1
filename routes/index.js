6ffe94bb53b26620b991ea02f9b639dfcc160fbb
valen <nicolas.andrada@hotmail.com>
2023-08-22 19:15:29 -0300

    a

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const {Usuario} = require('../models')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-                      title: 'Clases de Andrada', 
+                      title: 'Clases de siste comp', 
                       clase: 'Rutas y vistas'});
 });
 
-// router.get('/alumno', function(req, res, next){
-//   res.render('alumno')
-// })
+router.get('/alumno', function(req, res, next){
+  Usuario.create({nombre:'Nico', apellido:'Andrada'})
+  .then(usuario=>{
+    res.render('alumno')
+  })
+})
 
 module.exports = router;
